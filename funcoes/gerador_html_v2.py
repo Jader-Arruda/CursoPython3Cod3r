d906bf840734097d5aee3aafd4c5e88c099b5080
jader-arruda <gabrielsoaresprogramador@gmail.com>
2020-09-21 09:04:34 -0300

    Exercicios de python

---
+++ funcoes/gerador_html_v2.py
@@ -0,0 +1,14 @@
+#! Python
+
+
+def tag_bloco(texto, classe = 'success',inline = False):
+    tag ='span' if inline else 'div'
+    return f'<{tag} class="{classe}">{texto}</{tag}>'
+
+
+if __name__ == '__main__':
+    print(tag_bloco('bloco'))
+    print(tag_bloco('inline e classe', 'info',True))
+    print(tag_bloco('inline e classe', inline = True))
+    print(tag_bloco('falhou', classe='error'))
+
